6a074a4831cf18d8abf24197be7882a421859b5b
tamanugi <tamanugi@gmail.com>
2018-10-28 23:50:39 +0900

    implements flip logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,9 +40,62 @@ export class AppComponent implements OnInit {
     if (currCell !== 0) {
       return
     }
+  
+    if (this.flip(i, j, this.turn)) {
+      this.board[i][j] = this.turn
+      this.turn = 3 - this.turn 
+    }
+  }
 
-    this.board[i][j] = this.turn
+  private flip(y: number, x: number, myColor: number) {
+    let canFlip = false
 
-    this.turn = 3 - this.turn 
+    for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i == 0 && j == 0) {
+        continue
+      }
+
+      let _y = y + i
+      let _x = x + j
+
+      if (_y < 0 || _y >= 8 || _x < 0 || _x >= 8) {
+        continue
+      }
+
+      if (this.board[_y][_x] !== 3 - myColor) {
+        continue
+      }
+
+      if (this.checkFlip(_y, _x, i, j, myColor)) {
+        canFlip = true
+        while(this.board[_y][_x] == 3 - myColor) {
+          this.board[_y][_x] = myColor
+
+          _y += i
+          _x += j
+        }
+      }
+    }
+    }
+
+    return canFlip
+  }
+
+  private checkFlip(y: number, x: number, i: number, j: number, myColor: number) {
+
+    if (y < 0 || y >= 8 || x < 0 || x >= 8) {
+      return false
+    }
+
+    if (this.board[y][x] == 0) {
+      return false
+    }
+    if (this.board[y][x] == myColor) {
+      return true
+    }
+    if (this.board[y][x] == 3 - myColor) {
+      return this.checkFlip(y + i, x + j, i, j, myColor)
+    }
   }
 }
