20fe7c108590e4e06906843accffaea3781e87c3
tamanugi <tamanugi@gmail.com>
2018-10-28 23:18:03 +0900

    init board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
-  title = 'ng-othello';
+export class AppComponent implements OnInit {
+
+  board = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+  
+  ngOnInit() {
+  }
+
+  getCellClass(cell: number) {
+    const cellClass = {
+      0: "",
+      1: "white",
+      2: "black"
+    }
+
+    return cellClass[cell]
+  }
 }
